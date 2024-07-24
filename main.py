e20f86cb8573f43cc4d6e493033bef0ff4a06c16
Edward <onirotto7585@pm.me>
2024-07-24 13:23:14 +0700

    ten shapes finished

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+colors = ['medium aquamarine', 'spring green', 'navy', 'magenta', 'blue violet', 'yellow', 'lime', 'deep sky blue', 'sienna', 'light slate gray']
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for i in range(3,11):
+    tim.color(random.choice(colors))
+    draw_shape(i)
